97290e3cf0dbbd754b6ca78522af50d470e63119
jiseong0123 <kjs07965@gmail.com>
2026-05-15 10:50:20 +0900

    messsage

---
+++ 새 폴더 (2)/graph/graph/main.cpp
@@ -0,0 +1,32 @@
+#include "AdjMatGraph.h"
+
+
+void main()
+{
+	SrchAMGraph g;
+	for (int i = 0; i < 4; i++)
+		g.insertVertex('A' + i);
+	g.insertEdge(0, 1);
+	g.insertEdge(0, 3);
+	g.insertEdge(1, 2);
+	g.insertEdge(1, 3);
+	g.insertEdge(2, 3);
+	g.insertEdge(4, 5);
+	g.insertEdge(3, 4);
+	g.insertEdge(5, 6);
+	printf("인접행렬로 표현한 그래프\n");
+	g.display();
+
+	FILE* fp;
+	fopen_s(&fp, "C:/Users/USER/Desktop/data-structure-2026/new.txt","w");
+	g.display(fp);
+	fclose(fp);
+
+	printf("DFS ==>");
+	g.resetVisited();
+	g.DFS(0);
+	printf("\n");
+
+}
+
+
